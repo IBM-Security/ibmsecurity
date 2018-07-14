50a3970c63e7ce567e575854f716e61e23c13a69
sygilber <Sylvain.Gilbert@intact.net>
2018-07-13 20:50:47 -0400

    Provide candidate implementation for SNMP V3 Monitoring support

--- ibmsecurity/isam/base/snmp_monitoring.py
+++ ibmsecurity/isam/base/snmp_monitoring.py
@@ -34,11 +34,44 @@ def set_v1v2(isamAppliance, community, port=161, check_mode=False, force=False):
     return isamAppliance.create_return_object()
 
 
+def set_v3(isamAppliance, securityLevel, securityUser, authProtocol, authPassword, privacyProtocol, privacyPassword, port=161, check_mode=False, force=False):
+    """
+    Set SNMP Monitoring v3
+    """
+    if force is True or _check_v3(isamAppliance, securityLevel, securityUser, authProtocol, authPassword, privacyProtocol, privacyPassword, port) is False:
+        if check_mode is True:
+            return isamAppliance.create_return_object(changed=True)
+        else:
+            return isamAppliance.invoke_put(
+                "Updating SNMP Monitoring",
+                "/snmp/v1",
+                {
+                    "snmpv3": {
+                        "securityLevel": securityLevel,
+                        "securityUser": securityUser,
+                        "authProtocol": authProtocol,
+                        "authPassword": authPassword,
+                        "privacyProtocol": privacyProtocol,
+                        "privacyPassword": privacyPassword
+                    },
+                    "enabled": True,
+                    "port": port
+                })
+
+    return isamAppliance.create_return_object()
+
+
 def _check(isamAppliance, community, port):
     ret_obj = get(isamAppliance)
 
     if ret_obj['data']['enabled'] is False:
         logger.info("SNMP Monitoring is not enabled")
+        return False
+
+    if 'snmpv1v2c' in ret_obj['data']:
+        logger.info("SNMP Monitoring snmpv1v2c object is present")
+    else:
+        logger.info("SNMP Monitoring snmpv1v2c object is missing")
         return False
 
     if ret_obj['data']['snmpv1v2c']['community'] != community:
@@ -52,6 +85,50 @@ def _check(isamAppliance, community, port):
     return True
 
 
+def _check_v3(isamAppliance, securityLevel, securityUser, authProtocol, authPassword, privacyProtocol, privacyPassword, port):
+    ret_obj = get(isamAppliance)
+
+    if ret_obj['data']['enabled'] is False:
+        logger.info("SNMP Monitoring is not enabled")
+        return False
+
+    if ret_obj['data']['port'] != port:
+        logger.info("SNMP Monitoring port is different")
+        return False
+
+    if 'snmpv3' in ret_obj['data']:
+        logger.info("SNMP Monitoring snmpv3 object is present")
+    else:
+        logger.info("SNMP Monitoring snmpv3 object is missing")
+        return False
+
+    if ret_obj['data']['snmpv3']['securityLevel'] != securityLevel:
+        logger.info("SNMP Monitoring securityLevel is different")
+        return False
+
+    if ret_obj['data']['snmpv3']['securityUser'] != securityUser:
+        logger.info("SNMP Monitoring securityUser is different")
+        return False
+
+    if ret_obj['data']['snmpv3']['authProtocol'] != authProtocol:
+        logger.info("SNMP Monitoring authProtocol is different")
+        return False
+
+    if ret_obj['data']['snmpv3']['authPassword'] != authPassword:
+        logger.info("SNMP Monitoring authPassword is different")
+        return False
+
+    if ret_obj['data']['snmpv3']['privacyProtocol'] != privacyProtocol:
+        logger.info("SNMP Monitoring privacyProtocol is different")
+        return False
+
+    if ret_obj['data']['snmpv3']['privacyPassword'] != privacyPassword:
+        logger.info("SNMP Monitoring privacyPassword is different")
+        return False
+
+    return True
+
+    
 def disable(isamAppliance, check_mode=False, force=False):
     """
     Disable SNMP Monitoring
