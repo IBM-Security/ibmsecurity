206c4daf8d9584206440fc55c76953c52a307823
Lachlan Gleeson <lgleeson@au1.ibm.com>
2019-06-20 13:17:40 +1000

    Update file to read binary string

--- ibmsecurity/utilities/tools.py
+++ ibmsecurity/utilities/tools.py
@@ -161,9 +161,9 @@ def files_same(original_file, new_file):
         -works with text, image, and zip files
     Returns Boolean
     """
-    with open(original_file, 'r') as f:
+    with open(original_file, 'rb') as f:
         original_file_contents = f.read()
-    with open(new_file, 'r') as f:
+    with open(new_file, 'rb') as f:
         new_file_contents = f.read()
     hash_original_file = hashlib.sha224(original_file_contents).hexdigest()
     hash_new_file = hashlib.sha224(new_file_contents).hexdigest()
