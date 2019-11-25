c2e54a69e9019688de1b226e368f91e815caffd9
sygilber <sygilber@stha8n0fu.iad.ca.inet>
2019-11-25 13:53:28 -0500

    Add support for pendingChangesLifetime (9.0.7)

--- ibmsecurity/isam/base/admin.py
+++ ibmsecurity/isam/base/admin.py
@@ -47,7 +47,7 @@ def set(isamAppliance, oldPassword=None, newPassword=None, minHeapSize=None, max
         consoleLogLevel=None, acceptClientCerts=None, validateClientCertIdentity=None, excludeCsrfChecking=None,
         enableSSLv3=None, maxFiles=None, maxFileSize=None, enabledTLS=None, sshdPort=None, sessionCachePurge=None,
         sessionInactivityTimeout=None, sshdClientAliveInterval=None, swapFileSize=None, httpProxy=None,
-        enabledServerProtocols=None,loginHeader=None, loginMessage=None,
+        enabledServerProtocols=None,loginHeader=None, loginMessage=None, pendingChangesLifetime=None
         check_mode=False, force=False):
     """
     Updating the administrator settings
@@ -78,7 +78,7 @@ def _check(isamAppliance, oldPassword, newPassword, minHeapSize, maxHeapSize, se
            minThreads, maxThreads, maxPoolSize, lmiDebuggingEnabled, consoleLogLevel, acceptClientCerts,
            validateClientCertIdentity, excludeCsrfChecking, enableSSLv3, maxFiles, maxFileSize, enabledTLS, sshdPort,
            sessionCachePurge, sessionInactivityTimeout, sshdClientAliveInterval, swapFileSize, httpProxy,
-           enabledServerProtocols, loginHeader, loginMessage, warnings):
+           enabledServerProtocols, loginHeader, loginMessage, pendingChangesLifetime, warnings):
     """
     Check whether target key has already been set with the value
     :param isamAppliance:
@@ -264,7 +264,15 @@ def _check(isamAppliance, oldPassword, newPassword, minHeapSize, maxHeapSize, se
                 json_data["loginMessage"] = loginMessage
         elif 'loginMessage' in ret_obj['data']:
             del ret_obj['data']['loginMessage']
-
+        if pendingChangesLifetime is not None:
+            if ibmsecurity.utilities.tools.version_compare(isamAppliance.facts["version"], "9.0.7.0") < 0:
+                warnings.append(
+                    "Appliance at version: {0}, pendingChangesLifetime: {1} is not supported. Needs 9.0.7.0 or higher. Ignoring pendingChangesLifetime for this call.".format(
+                        isamAppliance.facts["version"], pendingChangesLifetime))
+            else:
+                json_data["pendingChangesLifetime"] = pendingChangesLifetime
+        elif 'pendingChangesLifetime' in ret_obj['data']:
+            del ret_obj['data']['pendingChangesLifetime']
 
     sorted_ret_obj = tools.json_sort(ret_obj['data'])
     sorted_json_data = tools.json_sort(json_data)
