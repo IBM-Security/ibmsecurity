61fc47c6d492a9dd5f3ec16dfae9a73f411ba9ba
Seshu Yerneni <syerneni@us.ibm.com>
2020-04-30 01:38:57 +0000

    Made code changes to differentiate Docker vs Appliance

--- ibmsecurity/isam/web/rsa_securid_config.py
+++ ibmsecurity/isam/web/rsa_securid_config.py
@@ -23,49 +23,50 @@ def upload(isamAppliance, filename, check_mode=False, force=False):
     """
     Upload a RSA Securid Config file
     """
-    ret_obj = get(isamAppliance)
-    warnings = ret_obj['warnings']
-    if warnings and 'Docker' in warnings[0]:
-        return isamAppliance.create_return_object(warnings=ret_obj['warnings'])
 
-    warnings = [
-        "Idempotency check is only to see if there was a config already uploaded. Force upload to replace existing configuration."]
+    srv_cfg_available, warnings = _check(isamAppliance)
+    if warnings and warnings[0] != '':
+        return isamAppliance.create_return_object(warnings=warnings)
 
-    if force is True or ret_obj['data']['server_config'] != 'available':
+    if force is True or srv_cfg_available is False:
+        warnings = [
+                "Idempotency check is only to see if there was a config already uploaded. Force upload to replace existing configuration."]
+
         if check_mode is True:
             return isamAppliance.create_return_object(changed=True, warnings=warnings)
         else:
             return isamAppliance.invoke_post_files(
-                "Upload a RSA Securid Config file",
-                "{0}/server_config".format(uri),
-                [
-                    {
-                        'file_formfield': 'server_config_file',
-                        'filename': filename,
-                        'mimetype': 'application/octet-stream'
-                    }
-                ],
-                {}, requires_modules=requires_modules,
-                requires_version=requires_version, warnings=warnings)
+                    "Upload a RSA Securid Config file",
+                    "{0}/server_config".format(uri),
+                    [
+                        dict(file_formfield='server_config_file', filename=filename,
+                             mimetype='application/octet-stream')
+                    ],
+                    {}, requires_modules=requires_modules,
+                    requires_version=requires_version, warnings=warnings, requires_model=requires_model)
 
-    return isamAppliance.create_return_object(warnings=ret_obj['warnings'])
+    return isamAppliance.create_return_object(warnings=warnings)
 
 
 def _check(isamAppliance):
     ret_obj = get(isamAppliance)
-    return ret_obj['data']['server_config'] == 'available'
+    srv_cfg_available, warnings = False, ret_obj['warnings']
+
+    if warnings == [] and ret_obj['data']['server_config'] == 'available':
+        srv_cfg_available = True
+    return srv_cfg_available, warnings
 
 
 def test(isamAppliance, username, passcode, check_mode=False, force=False):
     """
     Test RSA Configuration with username/passcode
     """
-    ret_obj = get(isamAppliance)
-    warnings = ret_obj['warnings']
-    if warnings and 'Docker' in warnings[0]:
-        return isamAppliance.create_return_object(warnings=ret_obj['warnings'])
+    srv_cfg_available, warnings = _check(isamAppliance)
 
-    if ret_obj['data']['server_config'] != 'available':
+    if warnings and warnings[0] != '':
+        return isamAppliance.create_return_object(warnings=warnings)
+
+    if srv_cfg_available is False:
         return isamAppliance.create_return_object(warnings=["Valid configuration not found, test skipped."])
     else:
         ret_obj = isamAppliance.invoke_post("Test RSA Configuration with username/passcode", "{0}/test".format(uri),
@@ -74,7 +75,9 @@ def test(isamAppliance, username, passcode, check_mode=False, force=False):
                                                 'passcode': passcode
                                             },
                                             requires_modules=requires_modules,
-                                            requires_version=requires_version, ignore_error=True)
+                                            requires_version=requires_version,
+                                            ignore_error=True,
+                                            requires_model=requires_model)
         if ret_obj['changed'] is True:
             ret_obj['changed'] = False
 
@@ -85,24 +88,22 @@ def delete(isamAppliance, check_mode=False, force=False):
     """
     Deleting or Clear RSA Securid Configuration
     """
-    ret_obj = get(isamAppliance)
-    warnings = ret_obj['warnings']
-    if warnings and 'Docker' in warnings[0]:
-        return isamAppliance.create_return_object(warnings=ret_obj['warnings'])
+    srv_cfg_available, warnings = _check(isamAppliance)
 
-    if force is True or ret_obj['data']['server_config'] == 'available':
+    if force is True or srv_cfg_available is True:
         if check_mode is True:
-            return isamAppliance.create_return_object(changed=True, warnings=ret_obj['warnings'])
+            return isamAppliance.create_return_object(changed=True, warnings=warnings)
         else:
             return isamAppliance.invoke_delete("Deleting or Clear RSA Securid Configuration",
                                                "{0}/server_config".format(uri),
                                                requires_modules=requires_modules,
-                                               requires_version=requires_version, requires_model=requires_model)
+                                               requires_version=requires_version,
+                                               requires_model=requires_model)
 
-    return isamAppliance.create_return_object(warnings=ret_obj['warnings'])
+    return isamAppliance.create_return_object(warnings=warnings)
 
 
-def clear(isamAppliance, check_mode=False, force=False, elseif=None):
+def clear(isamAppliance, check_mode=False, force=False):
     """
     Clear the node secret file
 
@@ -112,19 +113,16 @@ def clear(isamAppliance, check_mode=False, force=False, elseif=None):
     :return:
     """
     # TODO: This function has not been tested.  Please open an issue on GitHub if you find a problem.
+    srv_cfg_available, warnings = _check(isamAppliance)
 
-    ret_obj = get(isamAppliance)
-    warnings = ret_obj['warnings']
-    if warnings and 'Docker' in warnings[0]:
-        return isamAppliance.create_return_object(warnings)
-
-    if force is True or ret_obj['data']['server_config'] == 'available':
+    if force is True or srv_cfg_available is True:
         if check_mode is True:
-            return isamAppliance.create_return_object(changed=True, warnings=ret_obj['warnings'])
+            return isamAppliance.create_return_object(changed=True, warnings=warnings)
         else:
             return isamAppliance.invoke_delete("Clear the node secret file",
                                                "{0}/node_secret".format(uri),
                                                requires_modules=requires_modules,
-                                               requires_version=requires_version, requires_model=requires_model)
+                                               requires_version=requires_version,
+                                               requires_model=requires_model)
 
-    return isamAppliance.create_return_object(warnings=ret_obj['warnings'])
+    return isamAppliance.create_return_object(warnings=warnings)
