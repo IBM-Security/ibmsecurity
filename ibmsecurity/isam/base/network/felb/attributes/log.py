a975bdab8714fb2928150ce46614aaa380cc589b
Diana Jorgenson <djorgenson@ibm.com>
2020-05-18 16:49:29 -0500

    Added Docker compatibility to FELB APIs

--- ibmsecurity/isam/base/network/felb/attributes/log.py
+++ ibmsecurity/isam/base/network/felb/attributes/log.py
@@ -1,63 +1,66 @@
 import logging
+from ibmsecurity.utilities import tools
 
 logger = logging.getLogger(__name__)
 
 module_uri = "/isam/felb/configuration/logging"
 requires_modules = None
 requires_versions = None
+requires_model = "Appliance"
 
 
 def get(isamAppliance):
     """
-    Retrieves logging configuration attributes
+    Retrieving logging configuration attributes
     """
-    return isamAppliance.invoke_get("Retrieving logging configuration", module_uri)
+    return isamAppliance.invoke_get("Retrieving logging configuration attributes", module_uri, requires_model=requires_model)
 
 
 def update(isamAppliance, local, remote_address, remote_port, remote_facility, check_mode=False, force=False):
     """
-    Updates logging configuration
+    Updating logging configuration attributes
     """
-    change_required, json_data = _check(isamAppliance, local, remote_address, remote_port, remote_facility)
+
+    json_data = {
+        "local": local,
+        "remote_address": remote_address,
+        "remote_port": remote_port,
+        "remote_facility": remote_facility
+    }
+
+    change_required, warnings = _check(isamAppliance, json_data)
 
     if force is True or change_required is True:
-        return isamAppliance.invoke_put("Updating Configuration", module_uri, json_data,
-                                        requires_modules=requires_modules, requires_version=requires_versions)
-    if change_required is False:
-        return isamAppliance.create_return_object(changed=False)
+        if check_mode is True:
+            return isamAppliance.create_return_object(changed=True, warnings=warnings)
+        else:
+            return isamAppliance.invoke_put("Updating logging configuration attributes", module_uri, json_data,
+                                            requires_modules=requires_modules, requires_version=requires_versions,
+                                            requires_model=requires_model)
+
+    return isamAppliance.create_return_object(changed=False, warnings=warnings)
 
 
-def _check(isamAppliance, local, remote_address, remote_port, remote_facility):
+def _check(isamAppliance, json_data):
     """
     Check for idempotency
     """
-    check_obj = get(isamAppliance)
+    ret_obj = get(isamAppliance)
+    warnings = ret_obj['warnings']
     change_required = False
-    # If the configuration is local, remote entries are not used
-    if local is True:
-        json_data = {
-            "local": True,
-            "remote_address": "",
-            "remote_port": None,
-            "remote_facility": None
 
-        }
-        if check_obj['data']['local'] != local:
-            change_required = True
-        return change_required, json_data
+    if json_data['local'] is True:
+        if 'local' in ret_obj['data']:
+            if json_data['local'] != ret_obj['data']['local']:
+                change_required = True
     else:
-        if check_obj['data']['remote_address'] != remote_address:
-            change_required = True
-        if check_obj['data']['remote_port'] != remote_port:
-            change_required = True
-        if check_obj['data']['remote_facility'] != remote_facility:
+        sorted_ret_obj = tools.json_sort(ret_obj['data'])
+        sorted_json_data = tools.json_sort(json_data)
+
+        logger.debug("Sorted Existing Data:{0}".format(sorted_ret_obj))
+        logger.debug("Sorted Desired  Data:{0}".format(sorted_json_data))
+
+        if sorted_ret_obj != sorted_json_data:
             change_required = True
 
-        json_data = {
-            "local": local,
-            "remote_address": remote_address,
-            "remote_port": remote_port,
-            "remote_facility": remote_facility
-        }
-
-        return change_required, json_data
+    return change_required, warnings
