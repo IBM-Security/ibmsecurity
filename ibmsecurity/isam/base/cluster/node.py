ea43a0d8ffc638f78bccc3fc8b4bb9d09c4042d3
Diana Jorgenson <djorgenson@IBM-MacBook-Pro.local>
2020-06-16 16:03:30 -0500

    Updated setup.py version to 2020.06.16.3. Fixes for cluster node and configuration. #232

--- ibmsecurity/isam/base/cluster/node.py
+++ ibmsecurity/isam/base/cluster/node.py
@@ -101,14 +101,14 @@ def add(isamAppliance, signature_file, restricted=False, check_mode=False, force
         try:
             ret_obj = get_id(isamAppliance)
 
-            if 'Docker' in ' '.join(ret_obj['warnings'][0]):
+            if 'Docker' in ' '.join(ret_obj['warnings']):
                 return isamAppliance.create_return_object(warnings=ret_obj['warnings'])
             else:
                 id = ret_obj['data']['value']
         except IBMError:
             ret_obj = get_default_id(isamAppliance)
 
-            if 'Docker' in ' '.join(ret_obj['warnings'][0]):
+            if 'Docker' in ' '.join(ret_obj['warnings']):
                 return isamAppliance.create_return_object(warnings=ret_obj['warnings'])
             else:
                 id = ret_obj['data']['address']
