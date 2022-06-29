70a53325b7be1746288c9d5840f1a4fa11bd7a13
tbosmans <tom.bosmans@be.ibm.com>
2022-06-29 09:18:01 +0200

    fix: reorder parameters cfr. Diana

--- ibmsecurity/isam/web/reverse_proxy/junctions.py
+++ ibmsecurity/isam/web/reverse_proxy/junctions.py
@@ -90,8 +90,9 @@ def add(isamAppliance, reverseproxy_id, junction_point, server_hostname, server_
         client_ip_http=None, version_two_cookies=None, ltpa_keyfile=None, authz_rules=None, fsso_config_file=None,
         username=None, password=None, server_uuid=None, local_ip=None, ltpa_keyfile_password=None,
         delegation_support=None, scripting_support=None, insert_ltpa_cookies=None, check_mode=False, force=False,
-        http2_junction=None, http2_proxy=None, sni_name=None,
-        priority=None, server_cn=None, description=None, silent=None, warnings=[]):
+        http2_junction=None, http2_proxy=None, sni_name=None, description=None,
+        priority=None, server_cn=None, silent=None,
+        warnings=[]):
     """
     Creating a standard or virtual junction
 
@@ -644,7 +645,7 @@ def set(isamAppliance, reverseproxy_id, junction_point, server_hostname, server_
                    delegation_support=delegation_support, scripting_support=scripting_support,
                    insert_ltpa_cookies=insert_ltpa_cookies, check_mode=check_mode, force=True,
                    http2_junction=http2_junction, http2_proxy=http2_proxy, sni_name=sni_name, description=description,
-                   priority=priority, server_cn=server_cn,
+                   priority=priority, server_cn=server_cn, silent=silent,
                    warnings=warnings)
 
     return isamAppliance.create_return_object()
