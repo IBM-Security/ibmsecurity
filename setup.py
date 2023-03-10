5dc42859335e1e80789b53d80dd7f4eed0cd035f
Tom Bosmans <tom.bosmans@be.ibm.com>
2023-03-10 15:47:54 +0100

    reorganization: introduce pyproject.toml (pip will stop supporting setup.py soon)

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ setup(
     packages = find_packages(),
     # Date of release used for version - please be sure to use YYYY.MM.DD.seq#, MM and DD should be two digits e.g. 2017.02.05.0
     # seq# will be zero unless there are multiple release on a given day - then increment by one for additional release for that date
-    version = '2022.08.22.0',
+    version = '2023.3.10.2',
     description = 'Idempotent functions for IBM Security Appliance REST APIs',
     author='IBM',
     author_email='secorch@us.ibm.com',
     url='',
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Development Status :: 5 - Production/Stable',
@@ -23,6 +24,7 @@ setup(
     ],
     zip_safe = False,
     install_requires = [
-        'requests'
+        'requests',
+        'jmespath'
     ]
 )
