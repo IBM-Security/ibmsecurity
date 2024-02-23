a5b989a0f3d97c2d0bc19871d42bbbc0d6212683
Tom Bosmans <tom.bosmans@be.ibm.com>
2024-02-23 11:18:13 +0100

    feature: move variable to shared junctions_config.py file

--- ibmsecurity/isam/web/reverse_proxy/junctions_server.py
+++ ibmsecurity/isam/web/reverse_proxy/junctions_server.py
@@ -2,29 +2,12 @@ import logging
 import ibmsecurity.utilities.tools
 from ibmsecurity.utilities import tools
 import ibmsecurity.isam.web.reverse_proxy.junctions
+from ibmsecurity.isam.web.reverse_proxy.junctions_config import server_fields
 import json
 
 logger = logging.getLogger(__name__)
 
 uri = "/wga/reverseproxy"
-
-server_fields = {'server_hostname': {'type': 'string'},
-                 'server_port': {'type': 'number'},
-                 'case_sensitive_url': {'type': 'yesno', 'max_version': "10.0.6.0"},
-                 'case_insensitive_url': {'type': 'yesno', 'min_version': "10.0.6.0"},
-                 'http_port': {'type': 'number'},
-                 'local_ip': {'type': 'string'},
-                 'query_contents': {'type': 'string', 'alt_name': 'query_content_url'},
-                 'server_dn': {'type': 'string'},
-                 'server_uuid': {'type': 'ignore'},
-                 'virtual_hostname': {'type': 'string', 'alt_name': 'virtual_junction_hostname'},
-                 'windows_style_url': {'type': 'yesno'},
-                 'current_requests': {'type': 'ignore'},
-                 'total_requests': {'type': 'ignore'},
-                 'operation_state': {'type': 'ignore'},
-                 'server_state': {'type': 'ignore'},
-                 'priority': {'type': 'number', 'min_version': "10.0.2.0"},
-                 'server_cn': {'type': 'string', 'min_version': "10.0.2.0"}}
 
 def search(isamAppliance, reverseproxy_id, junction_point, server_hostname, server_port):
     ret_obj_new = isamAppliance.create_return_object()
