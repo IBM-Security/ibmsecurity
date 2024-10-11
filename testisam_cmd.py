e33993e130f20f011263f7a2259c93abe5840bea
Tom Bosmans <tom.bosmans@be.ibm.com>
2024-10-11 11:40:57 +0200

    fix: code reformatting

--- testisam_cmd.py
+++ testisam_cmd.py
@@ -21,7 +21,6 @@ from ibmsecurity.appliance.isamappliance import ISAMAppliance
 from ibmsecurity.user.applianceuser import ApplianceUser
 import pkgutil
 import importlib
-import json
 import ibmsecurity
 
 from docopt import docopt
@@ -39,7 +38,7 @@ def import_submodules(package, recursive=True):
         package = importlib.import_module(package)
     results = {}
     for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
-        full_name = package.__name__ + '.' + name
+        full_name = package.__name__ + "." + name
         results[full_name] = importlib.import_module(full_name)
         if recursive and is_pkg:
             results.update(import_submodules(full_name))
@@ -54,34 +53,30 @@ import_submodules(ibmsecurity)
 # logging.getLogger(__name__).addHandler(logging.NullHandler())
 logging.basicConfig()
 # Valid values are 'DEBUG', 'INFO', 'ERROR', 'CRITICAL'
-logLevel = 'DEBUG'
+logLevel = "DEBUG"
 DEFAULT_LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'standard': {
-            'format': '[%(asctime)s] [PID:%(process)d TID:%(thread)d] [%(levelname)s] [%(name)s] [%(funcName)s():%(lineno)s] %(message)s'
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "[%(asctime)s] [PID:%(process)d TID:%(thread)d] [%(levelname)s] [%(name)s] [%(funcName)s():%(lineno)s] %(message)s"
         },
     },
-    'handlers': {
-        'default': {
-            'level': logLevel,
-            'formatter': 'standard',
-            'class': 'logging.StreamHandler',
+    "handlers": {
+        "default": {
+            "level": logLevel,
+            "formatter": "standard",
+            "class": "logging.StreamHandler",
         },
     },
-    'loggers': {
-        '': {
-            'level': logLevel,
-            'handlers': ['default'],
-            'propagate': True
+    "loggers": {
+        "": {"level": logLevel, "handlers": ["default"], "propagate": True},
+        "requests.packages.urllib3.connectionpool": {
+            "level": "ERROR",
+            "handlers": ["default"],
+            "propagate": True,
         },
-        'requests.packages.urllib3.connectionpool': {
-            'level': 'ERROR',
-            'handlers': ['default'],
-            'propagate': True
-        }
-    }
+    },
 }
 logging.config.dictConfig(DEFAULT_LOGGING)
 
@@ -97,22 +92,22 @@ def load_args(__doc__):
     method = None
     _options = "isamAppliance=isam_server"
     commit = False
-    if args['--commit']:
+    if args["--commit"]:
         commit = True
-    if args['--hostname']:
-        hostname = args['--hostname']
+    if args["--hostname"]:
+        hostname = args["--hostname"]
     else:
         hostname = "127.0.0.1"
-    if args['--username']:
-        username = args['--username']
+    if args["--username"]:
+        username = args["--username"]
     else:
         username = "admin@local"
-    if args['--password']:
-        password = args['--password']
+    if args["--password"]:
+        password = args["--password"]
     else:
         password = "admin"
-    if args['--lmi_port']:
-        lmi_port = args['--lmi_port']
+    if args["--lmi_port"]:
+        lmi_port = args["--lmi_port"]
     else:
         lmi_port = "443"
     if args["--method"]:
@@ -120,10 +115,10 @@ def load_args(__doc__):
     if args["--method_options"]:
         _newoptions = args["--method_options"]
         # split in key/value pairs
-        d = dict(kv.split('=', 1) for kv in _newoptions.split(','))
+        d = dict(kv.split("=", 1) for kv in _newoptions.split(","))
         for k, v in d.items():
             logging.debug(f"VALUE: {v}")
-            if 'json.' in v:
+            if "json." in v:
                 _options = _options + "," + k + "=" + eval(v)
             else:
                 _options = _options + "," + k + "='" + str(v) + "'"
@@ -137,7 +132,9 @@ if __name__ == "__main__":
     This test program should not execute when imported, which would otherwise
     cause problems when generating the documentation.
     """
-    commit, hostname, username, password, lmi_port, isam_module, options = load_args(__doc__)
+    commit, hostname, username, password, lmi_port, isam_module, options = load_args(
+        __doc__
+    )
 
     # Create a user credential for ISAM appliance
     u = ApplianceUser(username=username, password=password)
@@ -145,11 +142,11 @@ if __name__ == "__main__":
     isam_server = ISAMAppliance(hostname=hostname, user=u, lmi_port=lmi_port)
 
     # Run the method with options
-    module_name, method_name = isam_module.rsplit('.', 1)
+    module_name, method_name = isam_module.rsplit(".", 1)
     mod = importlib.import_module(module_name)
     func_ptr = getattr(mod, method_name)  # Convert action to actual function pointer
     logging.debug(func_ptr)
-    func_call = 'func_ptr(' + options + ')'
+    func_call = "func_ptr(" + options + ")"
 
     # Execute requested 'action'
     p(eval(func_call))
