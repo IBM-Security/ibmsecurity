29c3189916911e6aa1dae4e6cff297b8bc9b3d9a
Tom Bosmans <tom.bosmans@be.ibm.com>
2024-10-11 16:13:43 +0200

    bug: does not work

--- ibmsecurity/isam/base/container_ext/health.py
+++ ibmsecurity/isam/base/container_ext/health.py
@@ -1,4 +1,5 @@
 import logging
+import ibmsecurity.isam.base.container_ext.container as container
 
 logger = logging.getLogger(__name__)
 
@@ -10,7 +11,7 @@ requires_model = "Appliance"
 
 def get_all(isamAppliance, check_mode=False, force=False):
     """
-    Retrieving health for all (running?) containers TODO: THIS FAILS
+    Retrieving health for all (running?) containers TODO: THIS FAILS IN 10.0.8
     ./testisam_cmd.py --hostname=${ISAM_LMI} --method=ibmsecurity.isam.base.container_ext.health.get_all
 
     """
@@ -22,13 +23,22 @@ def get_all(isamAppliance, check_mode=False, force=False):
     )
 
 
-def get(isamAppliance, container_id, check_mode=False, force=False):
+def get(isamAppliance, name=None, container_id=None, check_mode=False, force=False):
     """
-    Get health for a specific container
+    Get health for a specific container TODO: THIS FAILS IN 10.0.8
+    Supply either name or id.
+    ./testisam_cmd.py --hostname=${ISAM_LMI} --method=ibmsecurity.isam.base.container_ext.health.get --method_options="container_id=a6252734-87d3-11ef-bec6-000c29d80c72"
     """
-    return isamAppliance.invoke_get(
-        "Retrieving health for container",
-        f"{uri}/{container_id}",
-        requires_model=requires_model,
-        requires_version=requires_version,
-    )
+    if container_id is None and name is not None:
+        container_id = container.search(isamAppliance, name)
+        container_id = container_id.get('data', None)
+
+    if container_id is not None:
+        return isamAppliance.invoke_get(
+            "Retrieving health for container",
+            f"{uri}/{container_id}",
+            requires_model=requires_model,
+            requires_version=requires_version,
+        )
+    else:
+        return isamAppliance.create_return_object()
