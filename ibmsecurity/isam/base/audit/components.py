ed403b9241bb86ac4d9ab713a049479d73f3c53e
Tom Bosmans <tom.bosmans@be.ibm.com>
2024-12-06 16:50:46 +0100

    feature: implement audit components settings

--- ibmsecurity/isam/base/audit/components.py
+++ ibmsecurity/isam/base/audit/components.py
@@ -1,4 +1,5 @@
 import logging
+
 from ibmsecurity.utilities import tools
 
 try:
@@ -22,225 +23,110 @@ def get_all(isamAppliance, check_mode=False, force=False):
                                     requires_version=requires_version)
 
 
-def get(isamAppliance, check_mode=False, force=False):
+def search(isamAppliance, component_name: str, check_mode=False, force=False):
     """
-    Retrieve audit configuration components
+    Get the id for the component by (group) name
     """
-    return isamAppliance.invoke_get("Retrieve audit configuration component", uri, requires_modules=requires_modules,
-                                    requires_version=requires_version)
+    ret_obj = None
+
+    ret_obj = get_all(isamAppliance)
+    if ret_obj.get("data", None):
+        for obj in ret_obj.get("data"):
+            if obj['group'] == component_name:
+                logger.info(f"Found name {component_name} id: {obj['id']}")
+                return obj['id']
+        return None
+    else:
+        return None
 
 
-def set(isamAppliance, id, config, enabled=True, type='Syslog', verbose=True, check_mode=False, force=False, use_json=False, components=None):
+def get(isamAppliance, group_name: str = None, component_id: str = None, type_id: str = None, check_mode=False, force=False):
     """
-    Update Audit Configuration
-
-    Sample data for Audit Configuration:
-    In JSON Format:
-    { u'config': [ { u'datatype': u'String',
-                     u'key': u'ISAM.Audit.syslogclient.SSL_TRUST_STORE',
-                     u'sensitive': False,
-                     u'validValues': [],
-                     u'value': u''},
-                   { u'datatype': u'String',
-                     u'key': u'ISAM.Audit.syslogclient.CLIENT_AUTH_KEY',
-                     u'sensitive': False,
-                     u'validValues': [],
-                     u'value': u''},
-                   { u'datatype': u'Boolean',
-                     u'key': u'ISAM.Audit.syslogclient.FAILOVER_TO_DISK',
-                     u'sensitive': False,
-                     u'validValues': [],
-                     u'value': u'false'},
-                   { u'datatype': u'Integer',
-                     u'key': u'ISAM.Audit.syslogclient.NUM_RETRY',
-                     u'sensitive': False,
-                     u'validValues': [],
-                     u'value': u'2'},
-                   { u'datatype': u'Integer',
-                     u'key': u'ISAM.Audit.syslogclient.NUM_SENDER_THREADS',
-                     u'sensitive': False,
-                     u'validValues': [],
-                     u'value': u'1'},
-                   { u'datatype': u'Boolean',
-                     u'key': u'ISAM.Audit.syslogclient.CLIENT_CERT_AUTH_REQUIRED',
-                     u'sensitive': False,
-                     u'validValues': [],
-                     u'value': u'false'},
-                   { u'datatype': u'Integer',
-                     u'key': u'ISAM.Audit.syslogclient.SERVER_PORT',
-                     u'sensitive': False,
-                     u'validValues': [],
-                     u'value': u'514'},
-                   { u'datatype': u'Hostname',
-                     u'key': u'ISAM.Audit.syslogclient.SERVER_HOST',
-                     u'sensitive': False,
-                     u'validValues': [],
-                     u'value': u'127.0.0.1'},
-                   { u'datatype': u'String',
-                     u'key': u'ISAM.Audit.syslogclient.TRANSPORT',
-                     u'sensitive': False,
-                     u'validValues': [],
-                     u'value': u'TRANSPORT_UDP'},
-                   { u'datatype': u'Integer',
-                     u'key': u'ISAM.Audit.syslogclient.QUEUE_FULL_TIMEOUT',
-                     u'sensitive': False,
-                     u'validValues': [],
-                     u'value': u'-1'},
-                   { u'datatype': u'Integer',
-                     u'key': u'ISAM.Audit.syslogclient.MAX_QUEUE_SIZE',
-                     u'sensitive': False,
-                     u'validValues': [],
-                     u'value': u'1000'}],
-      u'enabled': False,
-      u'id': u'1',
-      u'type': u'Syslog',
-      u'verbose': False}
-
-    In YAML Format:
-    config:
-      - datatype: String
-        key: ISAM.Audit.syslogclient.SSL_TRUST_STORE
-        sensitive: false
-        validValues: []
-        value: ''
-      - datatype: String
-        key: ISAM.Audit.syslogclient.CLIENT_AUTH_KEY
-        sensitive: false
-        validValues: []
-        value: ''
-      - datatype: Boolean
-        key: ISAM.Audit.syslogclient.FAILOVER_TO_DISK
-        sensitive: false
-        validValues: []
-        value: 'false'
-      - datatype: Integer
-        key: ISAM.Audit.syslogclient.NUM_RETRY
-        sensitive: false
-        validValues: []
-        value: '2'
-      - datatype: Integer
-        key: ISAM.Audit.syslogclient.NUM_SENDER_THREADS
-        sensitive: false
-        validValues: []
-        value: '1'
-      - datatype: Boolean
-        key: ISAM.Audit.syslogclient.CLIENT_CERT_AUTH_REQUIRED
-        sensitive: false
-        validValues: []
-        value: 'false'
-      - datatype: Integer
-        key: ISAM.Audit.syslogclient.SERVER_PORT
-        sensitive: false
-        validValues: []
-        value: '514'
-      - datatype: Hostname
-        key: ISAM.Audit.syslogclient.SERVER_HOST
-        sensitive: false
-        validValues: []
-        value: 127.0.0.1
-      - datatype: String
-        key: ISAM.Audit.syslogclient.TRANSPORT
-        sensitive: false
-        validValues: []
-        value: TRANSPORT_UDP
-      - datatype: Integer
-        key: ISAM.Audit.syslogclient.QUEUE_FULL_TIMEOUT
-        sensitive: false
-        validValues: []
-        value: '-1'
-      - datatype: Integer
-        key: ISAM.Audit.syslogclient.MAX_QUEUE_SIZE
-        sensitive: false
-        validValues: []
-        value: '1000'
-    enabled: false
-    id: '1'
-    type: Syslog
-    verbose: false
+    Retrieve specific audit configuration component group by component_id, by type or by group name
     """
-    pol_id, update_required, json_data = _check(isamAppliance, id, config, enabled, type, verbose, use_json, components)
-    if pol_id is None:
-        from ibmsecurity.appliance.ibmappliance import IBMError
-        raise IBMError("999", "Cannot update data for unknown Audit Configuration ID: {0}".format(id))
+    requires_version = None
+    warnings = []
+    if component_id is None and type_id is None and group_name is None:
+        warnings = ['No group_name, component_id nor type_id passed']
+        return isamAppliance.create_return_object(warnings)
+    elif group_name:
+        # translate name to component_id
+        component_id = search(isamAppliance, component_name=group_name)
+        if component_id:
+            uri_part = component_id
+        else:
+            warnings = [f"Cannot find group by name of: {group_name}"]
+            return isamAppliance.create_return_object(warnings)
+    elif component_id:
+        # Ignore type_id in this case
+        uri_part = component_id
+    else:
+        # valid values are runtime or management
+        if type_id in ['runtime', 'management']:
+            requires_version = "10.0.7.0"
+            uri_part = type_id
+        else:
+            warnings = [f"Invalid type_id passed to function: {type_id}"]
+            return isamAppliance.create_return_object(warnings)
+    return isamAppliance.invoke_get("Retrieve audit configuration component ", f"{uri}/{uri_part}", requires_modules=requires_modules,
+                                    requires_version=requires_version, warnings=warnings)
 
-    if force is True or update_required is True:
-        if check_mode is True:
+
+def set(isamAppliance, component_id: str, enabled=True, check_mode=False, force=False):
+    """
+    Update Audit Configuration Component by id
+    This simply enables or disables the group.
+    """
+    if isinstance(enabled, str):
+        if enabled.upper() in ['TRUE', 'YES']:
+            enabled = True
+        else:
+            enabled = False
+    update_required = _check(isamAppliance, component_id, enabled)
+    if enabled:
+        json_data = {
+            'enabled': True
+        }
+    else:
+        json_data = {
+            'enabled': False
+        }
+    if force or update_required:
+        if check_mode:
             return isamAppliance.create_return_object(changed=True)
         else:
             return isamAppliance.invoke_put(
-                "Update Audit Configuration",
-                "{0}/{1}".format(uri, id), json_data, requires_modules=requires_modules,
+                "Update Audit Configuration Component",
+                f"{uri}/{component_id}",
+                json_data,
+                requires_modules=requires_modules,
                 requires_version=requires_version)
 
-    return isamAppliance.create_return_object()
 
-
-def _check(isamAppliance, id, config, enabled, type, verbose, use_json=False, components=None):
+def _check(isamAppliance, component_id: str, enabled: bool):
     """
     Check and return True if update needed
     """
     update_required = False
-    pol_id = None
-    # convert all values into string - any other type causes issues
-    for cfg in config:
-        if isinstance(cfg['value'], bool):
-            cfg['value'] = str(cfg['value']).lower()
-        else:
-            cfg['value'] = str(cfg['value'])
-    # Ensure boolean variables are set correctly
-    if isinstance(verbose, str):
-        if verbose.lower() == "true":
-            verbose = True
-        else:
-            verbose = False
-    if isinstance(enabled, str):
-        if enabled.lower() == "true":
-            enabled = True
-        else:
-            enabled = False
-    if isinstance(use_json, str):
-        if use_json.lower() == "true":
-            use_json = True
-        else:
-            use_json = False
-    json_data = {
-        "id": id,
-        "config": config,
-        "enabled": enabled,
-        "type": type,
-        "verbose": verbose,
-        "useJSONFormat": use_json
-    }
-    ret_obj = get(isamAppliance)
-    for aud_cfg in ret_obj['data']:
-        if id == aud_cfg['id']:
-            pol_id = id
-            break
-    if pol_id is None:
-        logger.warning("Audit Configuration not found, returning no update required.")
-        return pol_id, update_required, json_data
-    elif components is not None:
-        json_data["components"] = components
-        update_required = True
-    else:
-        import ibmsecurity.utilities.tools
-        sorted_json_data = ibmsecurity.utilities.tools.json_sort(json_data)
-        logger.debug("Sorted input: {0}".format(sorted_json_data))
-        sorted_ret_obj = ibmsecurity.utilities.tools.json_sort(aud_cfg)
-        logger.debug("Sorted existing data: {0}".format(sorted_ret_obj))
-        if sorted_ret_obj != sorted_json_data:
-            logger.info("Changes detected, update needed.")
-            update_required = True
 
-    return pol_id, update_required, json_data
+    ret_obj = get(isamAppliance, component_id=component_id)
+    cmp_cfg = ret_obj.get("data", None)
+    logger.debug(f"\n\n{cmp_cfg}\n\n")
+    if cmp_cfg is not None and str(cmp_cfg.get('enabled', "frottekop")) != str(enabled):
+        update_required = True
+        logger.debug(f"\n\nAudit Configuration Component requires an update {cmp_cfg.get('enabled')} <> {enabled}")
+    else:
+        logger.warning("Audit Configuration Component does not need an update or does not exist.")
+
+    return update_required
 
 
 def compare(isamAppliance1, isamAppliance2):
     """
-    Compare Audit Configuration between two appliances
+    Compare Audit Configuration Components between two appliances
     """
-    ret_obj1 = get(isamAppliance1)
-    ret_obj2 = get(isamAppliance2)
+    ret_obj1 = get_all(isamAppliance1)
+    ret_obj2 = get_all(isamAppliance2)
 
     for obj in ret_obj1['data']:
         del obj['id']
