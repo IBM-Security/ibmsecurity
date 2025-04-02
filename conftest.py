63410988b096607335844481b3dac9a3d655d795
Tom Bosmans <tom.bosmans@be.ibm.com>
2025-04-02 08:44:36 +0200

    feature: update test setup

--- conftest.py
+++ conftest.py
@@ -1,25 +1,27 @@
 """PyTest fixtures for testing the project."""
 from __future__ import annotations
 
+import os
+
 import pytest
 
 from ibmsecurity.appliance.isamappliance import ISAMAppliance
 from ibmsecurity.user.applianceuser import ApplianceUser
-# pylint: disable=wildcard-import,unused-wildcard-import
-
-
-class IviaLogin:
-    def __init__(self):
-        # Create a user credential for ISAM appliance
-        u = ApplianceUser(username="admin@local", password="admin")
-        # Create an ISAM appliance with above credential
-        self.isam_server = ISAMAppliance(hostname="192.168.1.11", user=u, lmi_port=443)
 
 
 @pytest.fixture
 def iviaServer():
-    s = IviaLogin()
-    return s.isam_server
+    """Initiate an ISAMAppliance."""
+    # s = IviaLogin()
+    _username = os.getenv('IVIA_ADMIN')
+    _pw = os.getenv('IVIA_PW')
+    _host = os.getenv('IVIA_HOST')
+    _port = os.getenv('IVIA_PORT') or 443
+    # Create a user credential for ISAM appliance
+    u = ApplianceUser(username=_username, password=_pw)
+    # Create an ISAM appliance with above credential
+    isam_server = ISAMAppliance(hostname=_host, user=u, lmi_port=_port)
+    return isam_server
 
 
 # ibmsecurity
