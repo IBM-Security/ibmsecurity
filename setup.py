b4196fb8de04a403e3540821bcad8611db2ad99f
Tom Bosmans <tom.bosmans@be.ibm.com>
2025-09-26 13:22:31 +0200

    build: don't include test directory

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ibmsecurity",
-    packages=find_packages(),
+    packages=find_packages(exclude=["test"]),
     # Date of release used for version - please be sure to use YYYY.MM.DD.seq#, MM and DD should be two digits e.g. 2017.02.05.0
     # seq# will be zero unless there are multiple release on a given day - then increment by one for additional release for that date
     version="2025.7.14.0",
