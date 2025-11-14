e1c33e5ce655f92b6512fb45eb01b6525a7c3ea2
Tom Bosmans <tom.bosmans@be.ibm.com>
2025-11-14 10:47:11 +0100

    feature: test personal certs

---
+++ test/test_base_ssl_personalcerts.py
@@ -0,0 +1,126 @@
+import logging
+
+import ibmsecurity.isam.base.ssl_certificates.personal_certificate
+import ibmsecurity.isam.appliance
+import pytest
+
+def getTestData():
+    testdata = [
+        {
+            "kdb_id": "pdsrv",
+            "label": "github.io",
+            "cert": """-----BEGIN CERTIFICATE-----
+MIIGhTCCBW2gAwIBAgIRAJB3NEFHMW75lZl2eur98bkwDQYJKoZIhvcNAQELBQAw
+gY8xCzAJBgNVBAYTAkdCMRswGQYDVQQIExJHcmVhdGVyIE1hbmNoZXN0ZXIxEDAO
+BgNVBAcTB1NhbGZvcmQxGDAWBgNVBAoTD1NlY3RpZ28gTGltaXRlZDE3MDUGA1UE
+AxMuU2VjdGlnbyBSU0EgRG9tYWluIFZhbGlkYXRpb24gU2VjdXJlIFNlcnZlciBD
+QTAeFw0yNTAzMDcwMDAwMDBaFw0yNjAzMDcyMzU5NTlaMBYxFDASBgNVBAMMCyou
+Z2l0aHViLmlvMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxKQLElVm
+JYKnZ9dmKMWrb4fy4BWFm658EQemS4hJgrt+1NFpL2tGVaFupVyV3vmKorCX3zej
+c7+gH8Ugpemmj9r5tk1NZ0SXXALTjvT2i03oSqjwCzkn+R1o0TYg+G7PyQ5pE18A
+E+K3VUcpch1f5RyBTEvE4+HUg4/6OpAIYFVznJ3yk8a+bo1i/HBp2MbtPzssSlT8
+mPLY76SETtKdwgIdY91MkTiJd1x0KJDM2GPKM7pNTc81NMSw6WBzsxg4PFbR+BCY
+82/sYu8iMy/AdYcUz72hh2DGXnVypzzV/LLgJ/VAP5m+md0lVH5KIG/cduDrajlo
+LQ4LKJktO4VmwQIDAQABo4IDUjCCA04wHwYDVR0jBBgwFoAUjYxexFStiuF36Zv5
+mwXhuAGNYeEwHQYDVR0OBBYEFBLwftAxb+SvNbWJ+0LZ7bcLk80EMA4GA1UdDwEB
+/wQEAwIFoDAMBgNVHRMBAf8EAjAAMB0GA1UdJQQWMBQGCCsGAQUFBwMBBggrBgEF
+BQcDAjBJBgNVHSAEQjBAMDQGCysGAQQBsjEBAgIHMCUwIwYIKwYBBQUHAgEWF2h0
+dHBzOi8vc2VjdGlnby5jb20vQ1BTMAgGBmeBDAECATCBhAYIKwYBBQUHAQEEeDB2
+ME8GCCsGAQUFBzAChkNodHRwOi8vY3J0LnNlY3RpZ28uY29tL1NlY3RpZ29SU0FE
+b21haW5WYWxpZGF0aW9uU2VjdXJlU2VydmVyQ0EuY3J0MCMGCCsGAQUFBzABhhdo
+dHRwOi8vb2NzcC5zZWN0aWdvLmNvbTCCAX4GCisGAQQB1nkCBAIEggFuBIIBagFo
+AHYAlpdkv1VYl633Q4doNwhCd+nwOtX2pPM2bkakPw/KqcYAAAGVbeysdQAABAMA
+RzBFAiEA+YIgsAqb2cqQVlF4JP2ERIVCH3RXdB7DjIPc6Ch5aK4CIHjqUoV7F5Mk
+fcIQcmdn7Z5UR8nYtPA2OLvYc3mCFcLuAHcAGYbUxyiqb/66A294Kk0BkarOLXIx
+D67OXXBBLSVMx9QAAAGVbeysDgAABAMASDBGAiEAjryAbXlHsXj/v4f7CWXJzDUX
+SUuvA5kRH3doh4WPUQcCIQC+nojCqhCn/ZupbnI50O1T3FSKBQu/LOZ33fApzLJW
+hQB1AMs49xWJfIShRF9bwd37yW7ymlnNRwppBYWwyxTDFFjnAAABlW3srDcAAAQD
+AEYwRAIgS98L1D2W8nzV3tIQ0R4UJWxwxb7I/TT6e9ly0nA0QsACIFpl7s/WA1Qm
+z1Vm8ZtihoNFubO/AiiaVGaeDQiznHFCMHsGA1UdEQR0MHKCCyouZ2l0aHViLmlv
+ggwqLmdpdGh1Yi5jb22CFyouZ2l0aHVidXNlcmNvbnRlbnQuY29tggpnaXRodWIu
+Y29tgglnaXRodWIuaW+CFWdpdGh1YnVzZXJjb250ZW50LmNvbYIOd3d3LmdpdGh1
+Yi5jb20wDQYJKoZIhvcNAQELBQADggEBAHksjTVCptW9CtbBXu+7J2cDDmKRz/EA
+kUyONuojOnKoI3d2f5DQDkqzu/gSj6B28YO3a4EYFktvwq3KnXAu9KzSM1ehlhtA
+lxlvjjGUgXvux7DjnBH40ItKiE723opeWVbm2WExdRPSckm/CDwshz2U3Sl3M3Wt
+v0xPuZJrg1tMIL58RqrS5PpFlAIIlEUC6dr+xVQrwLNcYXVVgvZsRSX/YbrzboLM
+gWhuDSQPcaeDGHcy7NxRZHmlpHz+/Ot067VuxjGqm9veKNGZMUdroS+ocxAJBXv3
+Z1NCCowvpZazNxKccQg7izYwd6HL70WMxCWFU0e70uw9KZqteG7SVcQ=
+-----END CERTIFICATE-----
+""",
+            "preserve_label": "true"
+        },
+        {
+            "kdb_id": "pdsrv",
+            "label": "github.io",
+            "cert": "test/scripts/github.io.txt",
+            "preserve_label": "true"
+        }
+    ]
+    return testdata
+
+
+@pytest.mark.parametrize("items", getTestData())
+def test_import_signer_cert(iviaServer, caplog, items) -> None:
+    """Get all admincfg options."""
+    caplog.set_level(logging.DEBUG)
+    logging.log(logging.INFO, items)
+    arg = {}
+    kdb_id = None
+    cert = None
+    label = None
+    preserve_label = 'false'
+    for k, v in items.items():
+        if k == 'kdb_id':
+            kdb_id = v
+            continue
+        if k == 'cert':
+            cert = v
+            continue
+        if k == 'label':
+            label = v
+            continue
+        if k == 'preserve_label':
+            preserve_label = v
+            continue
+        arg[k] = v
+    returnValue = ibmsecurity.isam.base.ssl_certificates.signer_certificate.import_cert(iviaServer,
+                                                                                        kdb_id,
+                                                                                        cert,
+                                                                                        label,
+                                                                                        preserve_label,
+                                                                                        **arg
+                                                                                        )
+    logging.log(logging.INFO, returnValue)
+
+    assert not returnValue.failed()
+
+
+def getTestDataSigners():
+    testdata = [
+        {"kdb_id": "pdsrv", "cert_id": "new_cert"},
+    ]
+    return testdata
+
+
+@pytest.mark.parametrize("items", getTestDataSigners())
+def test_get_signer_cert_not_existing(iviaServer, caplog,items) -> None:
+    """Get single signer."""
+    caplog.set_level(logging.DEBUG)
+    arg = {}
+    kdb_id = None
+    cert = None
+    for k, v in items.items():
+        if k == 'kdb_id':
+            kdb_id = v
+            continue
+        if k == 'cert_id':
+            cert = v
+            continue
+        arg[k] = v
+    returnValue = ibmsecurity.isam.base.ssl_certificates.signer_certificate.get(iviaServer,
+                                                  kdb_id,
+                                                  cert,
+                                                  **arg)
+    logging.log(logging.INFO, returnValue)
+    # Expect this to fail
+    assert returnValue.failed()
