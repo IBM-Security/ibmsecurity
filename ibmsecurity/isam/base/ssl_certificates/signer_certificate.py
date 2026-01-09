6ff0dd97b24dd300f8f58c66ea547d741b081105
Tom Bosmans <tom.bosmans@be.ibm.com>
2026-01-09 13:56:32 +0100

    fix: don't throw errors when preserve_label=true; also handle cert chains

--- ibmsecurity/isam/base/ssl_certificates/signer_certificate.py
+++ ibmsecurity/isam/base/ssl_certificates/signer_certificate.py
@@ -1,5 +1,7 @@
 import logging
 import os.path
+import re
+
 from ibmsecurity.utilities.tools import files_same, get_random_temp_dir, version_compare
 import shutil
 
@@ -166,7 +168,7 @@ def import_cert(isamAppliance, kdb_id, cert, label, preserve_label='false', chec
     """
     # Let's do some simple check
     # check if the string begins with -----BEGIN CERTIFICATE-----
-    # so simply check if it's a long string
+    #
     if cert.startswith('-----BEGIN CERTIFICATE-----'):
       if force or _check_import_string(isamAppliance, kdb_id, label, cert, check_mode=check_mode):
           if check_mode:
@@ -177,10 +179,28 @@ def import_cert(isamAppliance, kdb_id, cert, label, preserve_label='false', chec
               json_data['cert'] = cert
               json_data['operation'] = "import" # this is missing from the documentation.
               if version_compare(isamAppliance.facts['version'], "10.0.5.0") >= 0:
-                  json_data['preserve_label'] = preserve_label
-              return isamAppliance.invoke_post("Create signer cert from certificate string",
+                  json_data['preserve_label'] = preserve_label.lower()
+              ret_obj = isamAppliance.invoke_post("Create signer cert from certificate string",
                                                f"/isam/ssl_certificates/{kdb_id}/signer_cert",
-                                               json_data)
+                                               json_data,
+                                               ignore_error=True)
+              if preserve_label.lower() == 'true':
+                  if ret_obj['rc'] == 500:
+                      # this most likely means the certificate exists already (with a different name ?)
+                      if "DPWAP0134E" in ret_obj['data'].get('message', ''):
+                          return isamAppliance.create_return_object(rc=0, warnings=(f"Not updating existing certificate (preserve_label=true): {ret_obj.get('data', {}).get('message', '')}"), changed=False)
+                      # this most likely means the certificate exists already (with a different name ?)
+                      if "duplicate" in ret_obj['data'].get('message', ''):
+                          return isamAppliance.create_return_object(rc=0, warnings=(f"Not updating existing certificate (preserve_label=true): {ret_obj.get('data', {}).get('message', '')}"), changed=False)
+              else:
+                  if ret_obj['rc'] == 500:
+                      # this most likely means the certificate exists already (with a different name ?)
+                      if "DPWAP0134E" in ret_obj['data'].get('message', ''):
+                          return isamAppliance.create_return_object(rc=0, warnings=(f"Not updating existing certificate (preserve_label=false): {ret_obj.get('data', {}).get('message', '')}"), changed=False)
+              if ret_obj['rc'] == 404:
+                  logger.info(f"Not found... which is odd")
+              return ret_obj
+
     else:
       if force or _check_import(isamAppliance, kdb_id, label, cert, check_mode=check_mode):
           if check_mode:
@@ -199,7 +219,8 @@ def import_cert(isamAppliance, kdb_id, cert, label, preserve_label='false', chec
                       ],
                       {'label': label})
               else:
-                  return isamAppliance.invoke_post_files(
+                  # Preserve label true , throws 500 error
+                  ret_obj = isamAppliance.invoke_post_files(
                       "Importing a signer certificate into a certificate database",
                       f"/isam/ssl_certificates/{kdb_id}/signer_cert",
                       [
@@ -210,7 +231,31 @@ def import_cert(isamAppliance, kdb_id, cert, label, preserve_label='false', chec
                           }
                       ],
                       {'label': label,
-                       'preserve_label': preserve_label})
+                       'preserve_label': preserve_label.lower()},
+                      ignore_error=True
+                  )
+                  if preserve_label.lower() == 'true':
+                      if ret_obj['rc'] == 500:
+                          # this most likely means the certificate exists already (with a different name ?)
+                          if "DPWAP0134E" in ret_obj['data'].get('message', ''):
+                              return isamAppliance.create_return_object(rc=0, warnings=(
+                                  f"Not updating existing certificate (preserve_label=true): {ret_obj.get('data', {}).get('message', '')}"),
+                                                                        changed=False)
+                          # this most likely means the certificate exists already (with a different name ?)
+                          if "duplicate" in ret_obj['data'].get('message', ''):
+                              return isamAppliance.create_return_object(rc=0, warnings=(
+                                  f"Not updating existing certificate (preserve_label=true): {ret_obj.get('data', {}).get('message', '')}"),
+                                                                        changed=False)
+                  else:
+                      if ret_obj['rc'] == 500:
+                          # this most likely means the certificate exists already (with a different name ?)
+                          if "DPWAP0134E" in ret_obj['data'].get('message', ''):
+                              return isamAppliance.create_return_object(rc=0, warnings=(
+                                  f"Not updating existing certificate (preserve_label=false): {ret_obj.get('data', {}).get('message', '')}"),
+                                                                        changed=False)
+                  if ret_obj['rc'] == 404:
+                      logger.info(f"Not found... which is odd")
+                  return ret_obj
 
     return isamAppliance.create_return_object()
 
@@ -222,7 +267,7 @@ def _check(isamAppliance, kdb_id, cert_id):
     ret_obj = get_all(isamAppliance, kdb_id)
 
     for certdb in ret_obj['data']:
-        if certdb['id'] == cert_id:
+        if certdb.get('id', None) == cert_id:
             return True
 
     return False
@@ -249,16 +294,21 @@ def _check_import(isamAppliance, kdb_id, cert_id, filename, check_mode=False):
     filename may be a chain, in that case we need to compare a bit more
     """
     with open(filename) as file:
-        newcert = file.read()
+        newcert = file.read() # tuple
+        # split into separate certs , if it's a chain
+        newcert = re.findall(r'(-----[BEGIN \S\ ]+?-----[\S\s]+?-----[END \S\ ]+?-----)', newcert)
+
     cert_pem = get(isamAppliance, kdb_id, cert_id)
     cert_pem = cert_pem.get("data", {})
     cert_pem = cert_pem.get("contents", "")
     if cert_pem == "":
         # No certificate found.  Fine.
         return True
-    if cert_pem.replace(" ", "").replace("\n", "").replace("\r", "") == newcert.replace(" ", "").replace("\n", "").replace("\r", ""):
-        logger.debug(f"Signer Certificate already exists with same label {cert_id}")
-        return False
+
+    for nc in newcert:
+        if cert_pem.replace(" ", "").replace("\n", "").replace("\r", "") == nc.replace(" ", "").replace("\n", "").replace("\r", ""):
+            logger.debug(f"Signer Certificate already exists with same label {cert_id}")
+            return False
     return True
 
 
